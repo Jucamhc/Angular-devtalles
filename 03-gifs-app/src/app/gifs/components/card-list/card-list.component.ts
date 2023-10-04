4f9e54fcb221167371c7de65d1110a2406238fbb
Kamilo2251 <camilo2251@gmail.com>
2023-10-03 22:35:21 -0500

    inicio y esctrutura

---
+++ 03-gifs-app/src/app/gifs/components/card-list/card-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'card-list',
+  template: `
+  <ul>
+    <li>Primero</li>
+    <li>Segundo</li>
+    <li>Tercero</li>
+    <li>Cuarto</li>
+  </ul>`,
+})
+export class CardListComponent   {
+
+}
